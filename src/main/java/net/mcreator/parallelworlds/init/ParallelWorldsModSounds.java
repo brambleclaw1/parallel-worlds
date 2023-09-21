1a94cd58d78db3c98e7388c3c02deda65f5f73a2
brambleclaw1 <gavinhoeksema@gmail.com>
2023-09-20 19:23:17 -0600

    sync

--- src/main/java/net/mcreator/parallelworlds/init/ParallelWorldsModSounds.java
+++ src/main/java/net/mcreator/parallelworlds/init/ParallelWorldsModSounds.java
@@ -18,4 +18,5 @@ public class ParallelWorldsModSounds {
 	public static final RegistryObject<SoundEvent> MODULO = REGISTRY.register("modulo", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("parallel_worlds", "modulo")));
 	public static final RegistryObject<SoundEvent> FAR_LANDS = REGISTRY.register("far_lands", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("parallel_worlds", "far_lands")));
 	public static final RegistryObject<SoundEvent> ECHO_IN_THE_WIND_EPIC = REGISTRY.register("echo_in_the_wind_epic", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("parallel_worlds", "echo_in_the_wind_epic")));
+	public static final RegistryObject<SoundEvent> MINECRAFT_EPIC = REGISTRY.register("minecraft_epic", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("parallel_worlds", "minecraft_epic")));
 }
